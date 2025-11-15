75fc91b7a1a5a4b0d52e21dd6c4a8a8c47fab9ef
milliydev <zohidtojiyev071@gmail.com>
2025-11-15 15:13:31 +0500

    ./

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,13 @@
 <template>
   <div>
-    <LayoutHeader />
-    <main>
-      <AboutSection/>
-      <Testimoialsectin/>
-      <slot />
-]       <CWhychooseus/>
-      <COurservices/>
-      <BlogSection/>
-      <CommonCOurservices/>
-      <CommonCContact/>
-      <COurteam/>
-      <CCompanyLogo/>
 
-
-    </main>
-    <LayoutFooter />
   </div>
 </template>
-<script setup lang="ts">
-import AboutSection from  "~/components/Common/AboutSection.vue";
-import Testimoialsectin from "~/components/Common/Testimoialsectin.vue";
-import CWhychooseus from "~/components/Common/CWhychooseus.vue";
-import COurteam from "~/components/Common/COurteam.vue";
-import CCompanyLogo from "~/components/Common/CCompanyLogo.vue";
+
+<script setup>
+
 </script>
+
+<style lang="scss" scoped>
+
+</style>
